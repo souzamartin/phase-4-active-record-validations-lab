2a890f62c9e4b450afe7fa5acc858d3df07c0885
Martin <souzam@sonic.net>
2022-12-13 10:44:32 -0500

    commit tree acd55a2cb4f997eac7679da99fb4b2d07aec4ffc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    # validate :is_clickbait
+
+    # def is_clickbait
+    #     unless title.include?("Won't Believe" or "Secret" or "Top" or "Guess")
+    #         errors.add(:title, "Must be clickbait")
+    #     end
+    # end
 end
